9efd59651b58bb8730c537e5e4ca0b5f917577a9
Mario Nicolae <m.a.nicolae@student.tudelft.nl>
2026-03-10 12:04:46 +0200

    Initial project structure: Spring Boot backend + React/TS frontend

---
+++ backend/src/test/java/com/mario/pantry/PantryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mario.pantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
